6e59ae55a96e2c597fb2c129e2c1a133e5e0972a
Brian Page <bapage@lps.umd.edu>
2023-07-27 07:39:24 -0400

    compiler error fixes

--- src/memToRtr.h
+++ src/memToRtr.h
@@ -8,8 +8,8 @@
 #include "eventConverter.h"
 #include <sst/core/event.h>
 #include <sst/core/link.h>
-#include "/scratch/shared/sst/13.0.0/elements/include/sst/elements/memHierarchy/memEvent.h"
-#include "/scratch/shared/sst/13.0.0/elements/include/sst/elements/memHierarchy/memEventBase.h"
+#include "/scratch/shared/apps/sst/13.0.0/elements/include/sst/elements/memHierarchy/memEvent.h"
+#include "/scratch/shared/apps/sst/13.0.0/elements/include/sst/elements/memHierarchy/memEventBase.h"
 
 
 using namespace SST;
--- src/rtrToMem.h
+++ src/rtrToMem.h
@@ -8,7 +8,7 @@
 #include "eventConverter.h"
 #include <sst/core/event.h>
 #include <sst/core/link.h>
-#include "/scratch/shared/sst/13.0.0/elements/include/sst/elements/merlin/router.h"
+#include "/scratch/shared/apps/sst/13.0.0/elements/include/sst/elements/merlin/router.h"
 
 using namespace SST;
 using namespace SST::eventConverter;
