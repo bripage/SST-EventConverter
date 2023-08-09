6c2e3d794194a3a2f0fe4f5c816a0357cde23a40
bripage <bpage1@nd.edu>
2023-08-09 15:47:44 -0400

    working on init()

--- conversionTest.py
+++ conversionTest.py
@@ -142,7 +142,7 @@ router2.addParams({
   "input_buf_size" : "512B",
   "output_buf_size" : "512B",
   "link_bw" : "1GB/s",
-  "num_ports" : 3,
+  "num_ports" : 4,
   "mesh.local_ports" : 1,
   "mesh.shape" : "2",
   "mesh.width" : "1",
@@ -162,13 +162,13 @@ link_cpu_evConv = sst.Link("link_cpu_evConv")
 link_cpu_evConv.connect((l1_cache, "low_network_0", "1ps"),(cpu_evConv_mem, "memPort", "1ps"))
 
 link_cpuConv_rtr1 = sst.Link("link_cpuConv_rtr1")
-link_cpuConv_rtr1.connect((cpu_evConv_rtr_iFace, "rtr_port", "1ps"),(router1, "port2", "1ps"))
+link_cpuConv_rtr1.connect((cpu_evConv_rtr_iFace, "rtr_port", "1ps"),(router1, "port3", "1ps"))
 
 link_routers = sst.Link("link_routers")
 link_routers.connect((router1, "port0", "100ps"),(router2, "port0", "100ps"))
 
 link_rtr2_busConv = sst.Link("link_rtr1_busConv")
-link_rtr2_busConv.connect((router2, "port2", "1ps"), (bus_evConv_rtr_iFace, "rtr_port", "1ps"))
+link_rtr2_busConv.connect((router2, "port3", "1ps"), (bus_evConv_rtr_iFace, "rtr_port", "1ps"))
 
 link_mem_evConv = sst.Link("link_mem_evConv")
 link_mem_evConv.connect((memctrl, "direct_link", "1ps"),(bus_evConv_mem, "memPort", "1ps"))
