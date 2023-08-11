467beae8a1edd2c8b8b2283aea28ed99b0783de9
bripage <bpage1@nd.edu>
2023-08-11 08:53:48 -0400

    compilation error correction

--- src/eventConverter.cc
+++ src/eventConverter.cc
@@ -9,7 +9,7 @@ using namespace SST::Converter;
 
 eventConverter::eventConverter(ComponentId_t id, SST::Params& params)
 : Component(id) {
-    // Create a new SST output object
+    out->verbose(CALL_INFO, 9, 0, "%s starting component construction\n", getName().c_str());
     out = new Output("", 1, 0, Output::STDOUT);
 
     rtriface = loadUserSubComponent<SST::Interfaces::SimpleNetwork>("inFromRtr", ComponentInfo::SHARE_NONE, 1);
@@ -42,16 +42,6 @@ eventConverter::~eventConverter(){
 void eventConverter::init(unsigned int phase){
     out->verbose(CALL_INFO, 9, 0, "%s begining init phase %d\n", getName().c_str(), phase);
 
-    memiface->init(phase);
-
-    if( rtriface->isNetworkInitialized() ){
-        if( rtriface->isNetworkInitialized() ) {
-            rtriface->init(phase);
-        }
-        //rtriface->sendUntimedData(req);
-    }
-
-    out->verbose(CALL_INFO, 9, 0, "%s ending init phase %d\n", getName().c_str(), phase);
 }
 
 void eventConverter::memToRtr(SST::Interfaces::StandardMem::Request* ev){
