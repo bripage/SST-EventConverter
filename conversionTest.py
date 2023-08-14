7586a217ed48efe5ff9d1127c30e64aef2fb9d54
bripage <bpage1@nd.edu>
2023-08-14 10:20:10 -0400

    rev attempt

--- conversionTest.py
+++ conversionTest.py
@@ -1,106 +1,126 @@
-#
-# conversionTest.py
-#
-
+import os
 import sst
-from sst.merlin import *
-#from sst.memHierarchy import *
 
-DEBUG_L1 = 0
-DEBUG_MEM = 0
+DEBUG_L1 = 1
+DEBUG_MEM = 10
 DEBUG_LEVEL = 10
 VERBOSE = 10
 MEM_SIZE = 1024*1024*1024-1
 
-sst.setProgramOption("timebase", "1 ps")
+NODES = 1
+SOCKETS_PER_NODE = 1
+TILES_PER_SOCKET = 6
+CORES_PER_TILE = 64
 
-#-- set the `stop-at` option
-sst.setProgramOption("stop-at", "10s")
+cores = []
+core_counter = 0
+l1caches = []
+buses = []
+bus_counter = 0
+memctrls = []
+links = []
+link_counter = 0
 
-#
-# Create CPU and Memory Traffic Generator
-#
-cpu = sst.Component("cpu", "miranda.BaseCPU")
-cpu.addParams({
-  "verbose" : 0,
-  "clock" : "1GHz",
-  "printStats" : 1
-})
-gen = cpu.setSubComponent("generator", "miranda.Stencil3DBenchGenerator")
-gen.addParams({
-  "verbose" : 0,
-  "nx" : 30,
-  "ny" : 20,
-  "nz" : 10
-})
+for N in NODES:
+  for S in SOCKETS_PER_NODE:
+    for T in TILES_PER_SOCKET:
+      bus = sst.Component("bus_n" + N + "s" + S + "t" + T, "memHierarchy.Bus")
+      bus.addParams({
+        "bus_frequency" : "2GHz"
+      })
+      buses.append(bus)
 
-iface = cpu.setSubComponent("memory", "memHierarchy.standardInterface")
+      for C in CORES_PER_TILE:
+        core = sst.Component("cpu_n" + N + "s" + S + "t" + T + "c" + C, "revcpu.RevCPU")
+        core.addParams({
+          "verbose" : 6,                                # Verbosity
+          "numCores" : 1,                               # Number of cores
+          "clock" : "2.0GHz",                           # Clock
+          "memSize" : MEM_SIZE,                         # Memory size in bytes
+          "machine" : "[0:RV64G]",                      # Core:Config; RV64G for core 0
+          "startAddr" : "[0:0x00000000]",               # Starting address for core 0
+          "memCost" : "[0:1:10]",                       # Memory loads required 1-10 cycles
+          "program" : os.getenv("REV_EXE", "cache_test2.exe"),  # Target executable
+          "enable_memH" : 1,                            # Enable memHierarchy support
+          "splash" : 1                                  # Display the splash message
+        })
+        core.enableAllStatistics()
 
-#
-# Create MemHierarchy Components
-#
-l1_cache = sst.Component("l1_cache", "memHierarchy.Cache")
-l1_cache.addParams({
-  "access_latency_cycles" : "2",
-  "cache_frequency" : "2GHz",
-  "replacement_policy" : "lru",
-  "coherence_protocol" : "MESI",
-  "associativity" : "4",
-  "cache_line_size" : "64",
-  "prefetcher" : "cassini.StridePrefetcher",
-  "debug" : "1",
-  "l1" : "1",
-  "cache_size" : "16KB"
-})
-l1_cache.enableAllStatistics({"type":"sst.AccumulatorStatistic"})
+        # Create the RevMemCtrl subcomponent
+        comp_lsq = core.setSubComponent("memory", "revcpu.RevBasicMemCtrl");
+        comp_lsq.addParams({
+          "verbose"         : "10",
+          "clock"           : "2.0Ghz",
+          "max_loads"       : 64,
+          "max_stores"      : 64,
+          "max_flush"       : 64,
+          "max_llsc"        : 64,
+          "max_readlock"    : 64,
+          "max_writeunlock" : 64,
+          "max_custom"      : 64,
+          "ops_per_cycle"   : 64
+        })
+        comp_lsq.enableAllStatistics({"type":"sst.AccumulatorStatistic"})
+        cores.append(core)
 
+        iface = comp_lsq.setSubComponent("memIface", "memHierarchy.standardInterface")
+        iface.addParams({
+          "verbose" : VERBOSE
+        })
 
-#
-# Create Event Converters
-#
-eventConverter1 = sst.Component("eventConverter1", "eventConverter.eventConverter")
+        l1cache = sst.Component("l1cache", "memHierarchy.Cache")
+        l1cache.addParams({
+          "access_latency_cycles" : "4",
+          "cache_frequency" : "2 Ghz",
+          "replacement_policy" : "lru",
+          "coherence_protocol" : "MESI",
+          "associativity" : "4",
+          "cache_line_size" : "64",
+          "debug" : 1,
+          "debug_level" : DEBUG_LEVEL,
+          "verbose" : VERBOSE,
+          "L1" : "1",
+          "cache_size" : "16KiB"
+        })
+        l1caches.append(l1cache)
 
-eventConverter1_rtrif = eventConverter1.setSubComponent("rtriface", "merlin.linkcontrol")
-eventConverter1_rtrif.addParams({
-  "input_buf_size" : "512B",
-  "output_buf_size" : "512B",
-  "link_bw" : "1GB/s"
-})
+        link = sst.Link("link_L1_" + "cpun" + N + "s" + S + "t" + T + "c" + C)
+        link.connect( (iface, "port", "1ns"), (l1cache, "high_network_0", "1ns") )
+        links.append(link)
 
-#
-# Create  Routers
-#
-router1 = sst.Component("router1", "merlin.hr_router")
-router1.setSubComponent("topology", "merlin.mesh")
-router1.addParams({
-  "id" : 0,
-  "xbar_bw" : "10GB/s",
-  "flit_size" : "512B",
-  "input_buf_size" : "512B",
-  "output_buf_size" : "512B",
-  "link_bw" : "1GB/s",
-  "num_ports" : 4,
-  "mesh.local_ports" : 2,
-  "mesh.shape" : "1",
-  "mesh.width" : "1",
-  "debug" : 1
-})
+        link = sst.Link("link_cpun" + N + "s" + S + "t" + T + "c" + C + "_L1_" + "busn" + N + "s" + S + "t" + T)
+        link.connect( (l1cache, "low_network_0", "1ns"), (bus, "high_network_" + C, "1ns") )
+        links.append(link)
 
+        cores.append(core)
+        l1caches.append(l1cache)
+      #end CORE loop
+      buses.append(bus)
 
-#
-# Create Links
-#
-# CPU --- L1Cache --- Converter --- Rtr1 --- Rtr2 --- Converter --- Bus --- MemCtrl
-#
-cpu_l1_link = sst.Link("cpu_l1_link")
-cpu_l1_link.connect((iface, "port", "10ps"),(l1_cache, "high_network_0", "10ps"))
+      memctrl = sst.Component("memctrl_" + N + "s" + S + "t" + T, "memHierarchy.MemController")
+      memctrl.addParams({
+        "debug" : DEBUG_MEM,
+        "debug_level" : DEBUG_LEVEL,
+        "clock" : "2GHz",
+        "verbose" : VERBOSE,
+        "addr_range_start" : 0,
+        "addr_range_end" : MEM_SIZE,
+        "backing" : "malloc"
+      })
 
+      memory = memctrl.setSubComponent("backend", "memHierarchy.simpleMem")
+      memory.addParams({
+        "access_time" : "100ns",
+        "mem_size" : "8GB"
+      })
 
+      link = sst.Link("link_bus" + N + "s" + S + "t" + T + "_memctrl" + N + "s" + S + "t" + T)
+      link.connect( (bus, "low_network_0", "1ns"), (memctrl, "direct_link", "1ns") )
+      links.append(link)
 
-link_eConv_rtr1 = sst.Link("link_eConv_rtr1")
-link_eConv_rtr1.connect((eventConverter1_rtrif, "rtr_port", "1ps"),(router1, "port3", "1ps"))
+      memctrls.append(memctrl)
+    # end TILE loop
 
+  #end SOCKET loop
 
-sst.setStatisticLoadLevel(10)
-sst.setStatisticOutput("sst.statOutputConsole")
-sst.enableAllStatisticsForComponentType("merlin.hr_router")
+#end NODE loop
